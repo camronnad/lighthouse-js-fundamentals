6204cfe4828b4bcb6ac86810b31834efc68bf5b8
Camron <camronnaderi57@gmail.com>
2023-03-28 20:08:54 -0700

    wrote a for loop, a while loop, and a reversed while loop

---
+++ ingredients.js
@@ -0,0 +1,10 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+
+ingredients.reverse()
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
